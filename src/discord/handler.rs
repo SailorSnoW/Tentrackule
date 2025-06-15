d98151af55d90326827fc0ba08afe53e9c0b73fc
SailorSnoW <sailorsnow@pm.me>
2025-06-15 13:18:05 +0200

    add a helper welcome message on invite

--- src/discord/handler.rs
+++ src/discord/handler.rs
@@ -1,3 +1,5 @@
+use tracing::warn;
+
 use super::*;
 
 pub async fn event_handler(
@@ -12,6 +14,25 @@ pub async fn event_handler(
             data_about_bot.guilds.len()
         );
         ctx.set_activity(Some(ActivityData::playing("League of Legends")));
+    }
+
+    if let serenity::FullEvent::GuildCreate { guild, is_new } = event {
+        if matches!(is_new, Some(true)) {
+            let welcome_message = "👋 Thanks for using Tentrackule 🦑! ".to_owned()
+                + "You should first configure the alert channel with `/set_alert_channel` to be able to receive tracking alerts.";
+
+            if let Some(channel_id) = guild.system_channel_id {
+                if let Err(e) = channel_id.say(ctx, &welcome_message).await {
+                    warn!("⚠️ [DISCORD] Couldn't send the welcome message: {}", e);
+                }
+            } else if let Some(first_text) = guild
+                .channels
+                .values()
+                .find(|c| c.kind == serenity::ChannelType::Text)
+            {
+                let _ = first_text.id.say(ctx, &welcome_message).await;
+            }
+        }
     }
 
     Ok(())
